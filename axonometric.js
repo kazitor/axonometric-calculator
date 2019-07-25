8cfac14428e97957aa68ab03c8eba62024eb7f54
kazitor <32972585+kazitor@users.noreply.github.com>
2019-07-25 10:07:54 +1000

    refeactor/condense

--- axonometric.js
+++ axonometric.js
@@ -10,26 +10,25 @@ var inputs = {
 		document.getElementById('ratio_top'),
 	]
 }
-var data = {
-	angles: {
-		left: 100 * Math.PI/180,
-		right: 120 * Math.PI/180
-	},
-	ratio: {
-		top: 0.947733,
-		left:0.888742,
-		right:0.558517
-	}
-};
 var axis=null;
+
+if(!Math.deg) {
+	Math.deg=function deg(angle) {
+		return angle*180/this.PI;
+	}
+}
+if(!Math.rad) {
+	Math.rad=function rad(angle) {
+		return angle*this.PI/180;
+	}
+}
 
 if (canvas.getContext) {
 	var ctx = canvas.getContext('2d');
-	extendContext(ctx);
 
-	drawAxes();
+	updateRatio();
 } else {
-	console.log("no canvas support");
+	console.warn("no canvas support");
 }
 
 canvas.addEventListener('mousemove', handleMouse);
@@ -43,12 +42,12 @@ for (var i = 0; i < inputs.angle.length; i++) {
 	});
 }
 
-function draw(mouse) {
-	if (mouse.b & 1) {
+function handleMouse(e) {
+	if (e.buttons & 1) {
 		if (axis===null) {
-			axis = (mouse.x>100) ? 1 : -1;
+			axis = (e.offsetX>100) ? 1 : -1;
 		}
-		var angle = Math.atan2(100-mouse.y,mouse.x-100);
+		var angle = Math.atan2(100-e.offsetY,e.offsetX-100);
 		angle = axis * (Math.PI/2 - angle);
 		if (angle<0) angle+= 2*Math.PI;
 
@@ -60,66 +59,43 @@ function draw(mouse) {
 	}
 }
 
-function drawAxes() {
+function updateRatio() {
+	var angles = inputs.angle.map(function(input) {
+		return Math.rad(input.value);
+	});
+	var ratio = calcRatio(angles);
+
+	inputs.ratio[0].value = ratio.left;
+	inputs.ratio[1].value = ratio.right;
+	inputs.ratio[2].value = ratio.top;
+
 	ctx.clearRect(0, 0, 200, 200);
 
 	ctx.beginPath();
 	ctx.arc(100,100,5,0,2*Math.PI,true);
 	ctx.fill();
 
-	ctx.drawAxis(data.ratio.top, Math.PI/2);
-	ctx.drawAxis(data.ratio.left, data.angles.left +Math.PI/2);
-	ctx.drawAxis(data.ratio.right,Math.PI/2 - data.angles.right);
+	drawAxis(ratio.top, Math.PI/2);
+	drawAxis(ratio.left, angles[0] +Math.PI/2);
+	drawAxis(ratio.right,Math.PI/2 - angles[1]);
 }
 
-function handleMouse(e) {
-	draw({
-		x: e.offsetX,
-		y: e.offsetY,
-		b: e.buttons
-	});
+function drawAxis(r, t) {
+	length=100*r;
+	ctx.beginPath();
+	ctx.moveTo(100,100);
+	ctx.lineTo(100 + length * Math.cos(t), 100 + length * -Math.sin(t));
+	ctx.stroke();
 }
 
-function updateRatio() {
-	data.angles.left  = Math.rad(inputs.angle[0].value);
-	data.angles.right = Math.rad(inputs.angle[1].value);
-
-	data.ratio = calcRatio(data.angles.left, data.angles.right);
-
-	inputs.ratio[0].value = data.ratio.left;
-	inputs.ratio[1].value = data.ratio.right;
-	inputs.ratio[2].value = data.ratio.top;
-
-	drawAxes();
-}
-
-function calcRatio(left, right) {
-	var LT = Math.tan(right - Math.PI/2);
-	var RT = Math.tan(left  - Math.PI/2);
-	var CT = Math.tan(3*Math.PI/2 - left - right);
+function calcRatio(angles) {
+	var LT = Math.tan(angles[1] - Math.PI/2);
+	var RT = Math.tan(angles[0] - Math.PI/2);
+	var CT = Math.tan(3*Math.PI/2 - angles[0] - angles[1]);
 	
 	return {
-		right:  Math.sqrt(1 - CT*LT),
+		right: Math.sqrt(1 - CT*LT),
 		top:   Math.sqrt(1 - LT*RT),
-		left: Math.sqrt(1 - RT*CT)
+		left:  Math.sqrt(1 - RT*CT)
 	};
-}
-
-// Adds functionality to a canvas context
-function extendContext(ctx) {
-	// draws a line from centre with length r and angle t
-	ctx.drawAxis=function (r, t) {
-		length=100*r;
-		this.beginPath();
-		this.moveTo(100,100);
-		this.lineTo(100 + length * Math.cos(t), 100 + length * -Math.sin(t));
-		this.stroke();
-	}
-}
-
-Math.deg=function (angle) {
-	return angle*180/this.PI;
-}
-Math.rad=function (angle) {
-	return angle*this.PI/180;
 }
